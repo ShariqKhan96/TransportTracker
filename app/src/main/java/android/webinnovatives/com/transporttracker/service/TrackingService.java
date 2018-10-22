34862b69b40ecfe34f6563b27dc6f5dc10617cc8
shariq <shariqmack@gmail.com>
2018-10-22 03:55:24 -0700

    Tracking service implemented

--- app/src/main/java/android/webinnovatives/com/transporttracker/service/TrackingService.java
+++ app/src/main/java/android/webinnovatives/com/transporttracker/service/TrackingService.java
@@ -6,17 +6,35 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.webinnovatives.com.transporttracker.Manifest;
 import android.webinnovatives.com.transporttracker.R;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TrackingService extends Service {
 
     private static final String TAG = TrackingService.class.getSimpleName();
 
     @Override
-    public IBinder onBind(Intent intent) {return null;}
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
     @Override
     public void onCreate() {
@@ -51,11 +69,49 @@ public class TrackingService extends Service {
     };
 
     private void loginToFirebase() {
-        // Functionality coming next step
+        // Authenticate with Firebase, and request location updates
+        String email = getString(R.string.firebase_email);
+        String password = getString(R.string.firebase_password);
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.e(TAG, "firebase auth success");
+                    requestLocationUpdates();
+                } else {
+                    Log.e(TAG, "firebase auth failed due to " + task.getException().getMessage());
+                }
+            }
+        });
     }
 
     private void requestLocationUpdates() {
-        // Functionality coming next step
-    }
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        if (client == null) Log.e(TAG, " Client is null");
+        final String path = getString(R.string.firebase_path) + "/" + getString(R.string.transport_id);
+        int permission = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates and when an update is
+            // received, store the location in Firebase
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+                    Location location = locationResult.getLastLocation();
+                    Log.e(TAG, (location.getProvider()));
+                    if (location != null) {
+                        Log.d(TAG, "location update " + location);
+                        ref.setValue(location);
+                    }
+                }
+            }, null);
+        }
 
+    }
 }
